89e058aa6af44c4bfc8ab658183cabd5a8dd1e1d
adgalvan_meli <adriel.galvan@mercadolibre.com>
2025-07-01 19:53:49 -0300

    add sections di

--- internal/application/di/warehouse_dependencies.go
+++ internal/application/di/warehouse_dependencies.go
@@ -2,9 +2,7 @@ package di
 
 import (
 	handler "ProyectoFinal/internal/handler"
-	repositorySection "ProyectoFinal/internal/repository/section"
 	repository "ProyectoFinal/internal/repository/warehouse"
-	serviceSection "ProyectoFinal/internal/service/section"
 	service "ProyectoFinal/internal/service/warehouse"
 	"ProyectoFinal/pkg/models"
 )
@@ -14,11 +12,4 @@ func GetWarehouseHandler(db map[int]models.Warehouse) *handler.WarehouseHandler 
 	warehouseService := service.NewWarehouseService(warehouseRepository)
 	warehouseHandler := handler.NewWarehouseHandler(warehouseService)
 	return warehouseHandler
-}
-
-func GetSectionHandler(db map[int]models.Section) *handler.SectionDefault {
-	sectionRepository := repositorySection.NewSectionMap(db)
-	sectionService := serviceSection.NewSectionDefault(sectionRepository)
-	sectionHandler := handler.NewSectionDefault(sectionService)
-	return sectionHandler
 }
