022823be7846db11d08c3ea88724fa7c84648e9c
adgalvan_meli <adriel.galvan@mercadolibre.com>
2025-07-02 12:26:31 -0300

    fix: IDs

--- internal/repository/section/section_map.go
+++ internal/repository/section/section_map.go
@@ -11,12 +11,25 @@ func NewSectionMap(db map[int]models.Section) *SectionMap {
 		defaultDb = db
 	}
 
-	return &SectionMap{db: defaultDb, lastID: len(defaultDb)}
+	return &SectionMap{
+		db:        defaultDb,
+		idCounter: checkCounter(defaultDb),
+	}
 }
 
 type SectionMap struct {
-	db     map[int]models.Section
-	lastID int
+	db        map[int]models.Section
+	idCounter int
+}
+
+func checkCounter(data map[int]models.Section) int {
+	idCounter := 0
+	for _, section := range data {
+		if section.ID > idCounter {
+			idCounter = section.ID
+		}
+	}
+	return idCounter
 }
 
 func (r *SectionMap) GetAll() (s []models.Section, err error) {
@@ -36,8 +49,8 @@ func (r *SectionMap) GetByID(id int) (models.Section, error) {
 }
 
 func (r *SectionMap) Create(section models.Section) (s models.Section, err error) {
-	r.lastID++
-	section.ID = r.lastID
+	r.idCounter++
+	section.ID = r.idCounter
 	r.db[section.ID] = section
 	return
 }
