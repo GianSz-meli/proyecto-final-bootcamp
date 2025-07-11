7f3798fde93180b4959091a26e1e36ef44984454
estfernandez_meli <estiben.fernandez@mercadolibre.com.co>
2025-07-11 15:43:03 -0500

    refactor: seller repository mysql

---
+++ internal/repository/seller/seller_mysql.go
@@ -0,0 +1,112 @@
+package seller
+
+import (
+	"ProyectoFinal/internal/repository/utils"
+	pkgErrors "ProyectoFinal/pkg/errors"
+	"ProyectoFinal/pkg/models"
+	"database/sql"
+	"errors"
+)
+
+type SellerMysql struct {
+	db *sql.DB
+}
+
+func NewSellerRepository(db *sql.DB) SellerRepository {
+	return &SellerMysql{
+		db: db,
+	}
+}
+
+func (r *SellerMysql) Create(seller models.Seller) (models.Seller, error) {
+	result, err := r.db.Exec(SQL_CREATE, seller.Cid, seller.CompanyName, seller.Address, seller.Telephone, seller.LocalityId)
+	if err != nil {
+		return models.Seller{}, err
+	}
+	lastId, err := result.LastInsertId()
+	if err != nil {
+		return models.Seller{}, err
+	}
+	seller.Id = int(lastId)
+	return seller, nil
+}
+
+func (r *SellerMysql) GetById(id int) *models.Seller {
+	row := r.db.QueryRow(SQL_GET_BY_ID, id)
+	if err := row.Err(); err != nil {
+		return nil, err
+	}
+	var seller models.Seller
+
+	if err := utils.SellerScan(row, &seller); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			newError := pkgErrors.WrapErrNotFound("Seller", "id", id)
+			return nil, newError
+		}
+		return nil, err
+	}
+
+	return &seller, nil
+}
+
+func (r *SellerMysql) ExistsByCid(cid int) (bool, error) {
+	var exists bool
+
+	if err := r.db.QueryRow(SQL_EXIST_BY_CID, cid).Scan(&exists); err != nil {
+		return false, err
+	}
+	return exists, nil
+}
+
+func (r *SellerMysql) GetAll() ([]models.Seller, error) {
+	rows, err := r.db.Query(SQL_GET_ALL)
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	var sellers []models.Seller
+
+	for rows.Next() {
+		var seller models.Seller
+		if err := rows.Scan(
+			&seller.Id,
+			&seller.Cid,
+			&seller.CompanyName,
+			&seller.Address,
+			&seller.Telephone,
+			&seller.LocalityId,
+		); err != nil {
+			return err
+		}
+		sellers = append(sellers, seller)
+	}
+
+	return sellers, nil
+}
+
+func (r *SellerMysql) Delete(id int) error {
+	_, err := r.db.Exec(SQL_DELETE, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *SellerMysql) Update(seller *models.Seller) (models.Seller, error) {
+
+	result, err := r.db.Exec(SQL_UPDATE, seller.Cid, seller.CompanyName, seller.Address, seller.Telephone, seller.LocalityId, seller.Id)
+	if err != nil {
+		return models.Seller{}, err
+	}
+	lastId, err := result.LastInsertId()
+	if err != nil {
+		return models.Seller{}, err
+	}
+	seller.Id = int(lastId)
+	return *seller, nil
+}
--- internal/repository/seller/seller_repository.go
+++ internal/repository/seller/seller_repository.go
@@ -3,10 +3,10 @@ package seller
 import "ProyectoFinal/pkg/models"
 
 type SellerRepository interface {
-	Create(seller *models.Seller)
-	GetById(id int) (models.Seller, bool)
-	ExistsByCid(cid int) bool
-	Update(seller *models.Seller)
-	GetAll() []models.Seller
-	Delete(id int)
+	Create(seller models.Seller) (models.Seller, error)
+	GetById(id int) (*models.Seller, error)
+	ExistsByCid(cid int) (bool, error)
+	Update(seller *models.Seller) (models.Seller, error)
+	GetAll() ([]models.Seller, error)
+	Delete(id int) error
 }
