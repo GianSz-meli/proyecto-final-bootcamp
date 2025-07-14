6c72dd667bda9e0198233f17c69373ac519691cb
adgalvan_meli <adriel.galvan@mercadolibre.com>
2025-07-14 17:36:32 -0300

    refactor: move SQL queries to const file and use log errors

--- internal/repository/section/section_mysql.go
+++ internal/repository/section/section_mysql.go
@@ -4,7 +4,7 @@ import (
 	"ProyectoFinal/pkg/models"
 	"database/sql"
 	"errors"
-	"fmt"
+	"log"
 )
 
 type SectionMySQL struct {
@@ -16,9 +16,9 @@ func NewSectionMySQL(db *sql.DB) *SectionMySQL {
 }
 
 func (r *SectionMySQL) GetAll() ([]models.Section, error) {
-	rows, err := r.db.Query(`SELECT id, section_number, current_temperature, minimum_temperature, current_capacity, minimum_capacity, product_type_id, warehouse_id, maximum_capacity FROM sections`)
+	rows, err := r.db.Query(SectionSelectAll)
 	if err != nil {
-		fmt.Println("[SectionMySQL][GetAll] error en Query:", err)
+		log.Println("[SectionMySQL][GetAll] error en Query:", err)
 		return nil, err
 	}
 	defer rows.Close()
@@ -28,7 +28,7 @@ func (r *SectionMySQL) GetAll() ([]models.Section, error) {
 		var s models.Section
 		err := rows.Scan(&s.ID, &s.SectionNumber, &s.CurrentTemperature, &s.MinimumTemperature, &s.CurrentCapacity, &s.MinimumCapacity, &s.ProductTypeID, &s.WarehouseID, &s.MaximumCapacity)
 		if err != nil {
-			fmt.Println("[SectionMySQL][GetAll] error en Scan:", err)
+			log.Println("[SectionMySQL][GetAll] error en Scan:", err)
 			return nil, err
 		}
 		sections = append(sections, s)
@@ -38,7 +38,7 @@ func (r *SectionMySQL) GetAll() ([]models.Section, error) {
 
 func (r *SectionMySQL) GetById(id int) (models.Section, bool) {
 	var s models.Section
-	err := r.db.QueryRow(`SELECT id, section_number, current_temperature, minimum_temperature, current_capacity, minimum_capacity, product_type_id, warehouse_id, maximum_capacity FROM sections WHERE id = ?`, id).Scan(
+	err := r.db.QueryRow(SectionSelectById, id).Scan(
 		&s.ID, &s.SectionNumber, &s.CurrentTemperature, &s.MinimumTemperature, &s.CurrentCapacity, &s.MinimumCapacity, &s.ProductTypeID, &s.WarehouseID, &s.MaximumCapacity,
 	)
 	if err != nil {
@@ -53,7 +53,7 @@ func (r *SectionMySQL) GetById(id int) (models.Section, bool) {
 func (r *SectionMySQL) Create(section models.Section) (models.Section, error) {
 	tx, err := r.db.Begin()
 	if err != nil {
-		fmt.Println("[SectionMySQL][Create] error en Begin:", err)
+		log.Println("[SectionMySQL][Create] error en Begin:", err)
 		return section, err
 	}
 	defer func() {
@@ -61,22 +61,22 @@ func (r *SectionMySQL) Create(section models.Section) (models.Section, error) {
 			tx.Rollback()
 		}
 	}()
-	res, err := tx.Exec(`INSERT INTO sections (section_number, current_temperature, minimum_temperature, current_capacity, minimum_capacity, product_type_id, warehouse_id, maximum_capacity) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+	res, err := tx.Exec(SectionInsert,
 		section.SectionNumber, section.CurrentTemperature, section.MinimumTemperature, section.CurrentCapacity, section.MinimumCapacity, section.ProductTypeID, section.WarehouseID, section.MaximumCapacity)
 	if err != nil {
-		fmt.Println("[SectionMySQL][Create] error en Exec:", err)
+		log.Println("[SectionMySQL][Create] error en Exec:", err)
 		tx.Rollback()
 		return section, err
 	}
 	id, err := res.LastInsertId()
 	if err != nil {
-		fmt.Println("[SectionMySQL][Create] error en LastInsertId:", err)
+		log.Println("[SectionMySQL][Create] error en LastInsertId:", err)
 		tx.Rollback()
 		return section, err
 	}
 	section.ID = int(id)
 	if err := tx.Commit(); err != nil {
-		fmt.Println("[SectionMySQL][Create] error en Commit:", err)
+		log.Println("[SectionMySQL][Create] error en Commit:", err)
 		return section, err
 	}
 	return section, nil
@@ -92,7 +92,7 @@ func (r *SectionMySQL) Update(id int, section models.Section) (models.Section, e
 			tx.Rollback()
 		}
 	}()
-	_, err = tx.Exec(`UPDATE sections SET section_number=?, current_temperature=?, minimum_temperature=?, current_capacity=?, minimum_capacity=?, product_type_id=?, warehouse_id=?, maximum_capacity=? WHERE id=?`,
+	_, err = tx.Exec(SectionUpdate,
 		section.SectionNumber, section.CurrentTemperature, section.MinimumTemperature, section.CurrentCapacity, section.MinimumCapacity, section.ProductTypeID, section.WarehouseID, section.MaximumCapacity, id)
 	if err != nil {
 		tx.Rollback()
@@ -115,7 +115,7 @@ func (r *SectionMySQL) Delete(id int) error {
 			tx.Rollback()
 		}
 	}()
-	_, err = tx.Exec(`DELETE FROM sections WHERE id=?`, id)
+	_, err = tx.Exec(SectionDelete, id)
 	if err != nil {
 		tx.Rollback()
 		return err
@@ -125,6 +125,6 @@ func (r *SectionMySQL) Delete(id int) error {
 
 func (r *SectionMySQL) ExistBySectionNumber(sectionNumber int) bool {
 	var exists bool
-	err := r.db.QueryRow(`SELECT EXISTS(SELECT 1 FROM sections WHERE section_number = ?)`, sectionNumber).Scan(&exists)
+	err := r.db.QueryRow(SectionExistsByNumber, sectionNumber).Scan(&exists)
 	return err == nil && exists
 }
