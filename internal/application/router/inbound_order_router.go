6aa8e67468c3ac386c61a05634bbcc763c717cd1
omolinaspublic <octavio.molinas@mercadolibre.com>
2025-07-15 16:50:40 -0300

    feat: add InboundOrder router

---
+++ internal/application/router/inbound_order_router.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	"ProyectoFinal/internal/handler"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func InboundOrderRoutes(ctr *handler.InboundOrderHandler) chi.Router {
+	r := chi.NewRouter()
+	r.Post("/", ctr.Create())
+	return r
+}
+
+func EmployeeInboundOrderReportRoutes(ctr *handler.InboundOrderHandler) chi.Router {
+	r := chi.NewRouter()
+	r.Get("/reportInboundOrders", ctr.GetEmployeeInboundOrdersReport())
+	return r
+}
