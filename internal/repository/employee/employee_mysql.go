a086e8cffb0138e498715e3b48eb5a3f3d90b07a
omolinaspublic <octavio.molinas@mercadolibre.com>
2025-07-15 12:55:06 -0300

    feat: integrate HandleMysqlError

--- internal/repository/employee/employee_mysql.go
+++ internal/repository/employee/employee_mysql.go
@@ -20,12 +20,12 @@ func NewMySQLRepository(db *sql.DB) Repository {
 func (r *mysqlRepository) GetAll() ([]models.Employee, error) {
 	rows, err := r.db.Query(QueryGetAllEmployees)
 	if err != nil {
-		return nil, err
+		return nil, pkgErrors.HandleMysqlError(err)
 	}
 	defer rows.Close()
 
 	if err = rows.Err(); err != nil {
-		return nil, err
+		return nil, pkgErrors.HandleMysqlError(err)
 	}
 
 	var employees []models.Employee
@@ -34,7 +34,7 @@ func (r *mysqlRepository) GetAll() ([]models.Employee, error) {
 
 		err := rows.Scan(&employee.ID, &employee.CardNumberID, &employee.FirstName, &employee.LastName, &employee.WarehouseID)
 		if err != nil {
-			return nil, err
+			return nil, pkgErrors.HandleMysqlError(err)
 		}
 
 		employees = append(employees, employee)
@@ -53,7 +53,7 @@ func (r *mysqlRepository) GetById(id int) (models.Employee, error) {
 		if errors.Is(err, sql.ErrNoRows) {
 			return models.Employee{}, pkgErrors.WrapErrNotFound("employee", "id", id)
 		}
-		return models.Employee{}, err
+		return models.Employee{}, pkgErrors.HandleMysqlError(err)
 	}
 
 	return employee, nil
@@ -62,12 +62,12 @@ func (r *mysqlRepository) GetById(id int) (models.Employee, error) {
 func (r *mysqlRepository) Create(employee *models.Employee) error {
 	result, err := r.db.Exec(QueryCreateEmployee, employee.CardNumberID, employee.FirstName, employee.LastName, employee.WarehouseID)
 	if err != nil {
-		return err
+		return pkgErrors.HandleMysqlError(err)
 	}
 
 	lastInsertID, err := result.LastInsertId()
 	if err != nil {
-		return err
+		return pkgErrors.HandleMysqlError(err)
 	}
 
 	employee.ID = int(lastInsertID)
@@ -79,7 +79,7 @@ func (r *mysqlRepository) ExistsByCardNumberId(cardNumberId string) (bool, error
 
 	err := r.db.QueryRow(QueryExistsByCardNumberId, cardNumberId).Scan(&exists)
 	if err != nil {
-		return false, err
+		return false, pkgErrors.HandleMysqlError(err)
 	}
 
 	return exists, nil
@@ -88,12 +88,12 @@ func (r *mysqlRepository) ExistsByCardNumberId(cardNumberId string) (bool, error
 func (r *mysqlRepository) Update(id int, employee models.Employee) error {
 	result, err := r.db.Exec(QueryUpdateEmployee, employee.CardNumberID, employee.FirstName, employee.LastName, employee.WarehouseID, id)
 	if err != nil {
-		return err
+		return pkgErrors.HandleMysqlError(err)
 	}
 
 	rowsAffected, err := result.RowsAffected()
 	if err != nil {
-		return err
+		return pkgErrors.HandleMysqlError(err)
 	}
 
 	if rowsAffected == 0 {
@@ -106,12 +106,12 @@ func (r *mysqlRepository) Update(id int, employee models.Employee) error {
 func (r *mysqlRepository) Delete(id int) error {
 	result, err := r.db.Exec(QueryDeleteEmployee, id)
 	if err != nil {
-		return err
+		return pkgErrors.HandleMysqlError(err)
 	}
 
 	rowsAffected, err := result.RowsAffected()
 	if err != nil {
-		return err
+		return pkgErrors.HandleMysqlError(err)
 	}
 
 	if rowsAffected == 0 {
