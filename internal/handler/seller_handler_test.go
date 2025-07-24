91641563b0c714d6ba388f6b36084eadf812bc3a
estfernandez_meli <estiben.fernandez@mercadolibre.com.co>
2025-07-24 10:38:43 -0500

    testing/ add test for handler getAll

--- internal/handler/seller_handler_test.go
+++ internal/handler/seller_handler_test.go
@@ -335,3 +335,79 @@ func TestSellerHandler_Create_Success(t *testing.T) {
 		})
 	}
 }
+
+// Test for Get All
+func TestSellerHandler_GetAll(t *testing.T) {
+	tests := []struct {
+		name       string
+		service    *mocks.MockSellerService
+		assertFunc func(t *testing.T, response *httptest.ResponseRecorder)
+	}{
+		{
+			name: "should return 200  when service returns all sellers",
+			service: &mocks.MockSellerService{
+				GetAllFunc: func() ([]models.Seller, error) {
+					sellers := []models.Seller{
+						{
+							Id:          1,
+							Cid:         "GDJ2SJ3",
+							CompanyName: "Farm to Table Produce Hub",
+							Address:     "812 Cypress Way, Denver, CO 80201",
+							Telephone:   "+1-555-1901",
+							LocalityId:  1,
+						},
+						{
+							Id:          2,
+							Cid:         "AD3HF4",
+							CompanyName: "Farm to Table Produce Hub Two",
+							Address:     "82 House Way, Alaska, CO 23531",
+							Telephone:   "+5-165-7201",
+							LocalityId:  1,
+						},
+					}
+					return sellers, nil
+				},
+			},
+			assertFunc: func(t *testing.T, response *httptest.ResponseRecorder) {
+				expectedBody := `{"data":[{"id":1,"cid":"GDJ2SJ3","company_name":"Farm to Table Produce Hub","address":"812 Cypress Way, Denver, CO 80201","telephone":"+1-555-1901","locality_id":1},{"id":2,"cid":"AD3HF4","company_name":"Farm to Table Produce Hub Two","address":"82 House Way, Alaska, CO 23531","telephone":"+5-165-7201","locality_id":1}]}`
+				expectedCode := http.StatusOK
+				require.Equal(t, expectedCode, response.Code)
+				require.JSONEq(t, expectedBody, response.Body.String())
+			},
+		},
+		{
+			name: "should return 500 internal server error error when service returns a mysql error not mapped",
+			service: &mocks.MockSellerService{
+				GetAllFunc: func() ([]models.Seller, error) {
+					mysqlError := &mysql.MySQLError{
+						Number:  1054,
+						Message: "Unknown column 'xxx' in 'xxx'",
+					}
+					return nil, mysqlError
+				},
+			},
+			assertFunc: func(t *testing.T, response *httptest.ResponseRecorder) {
+				expectedCode := http.StatusInternalServerError
+				require.Contains(t, response.Body.String(), http.StatusText(expectedCode))
+				require.Equal(t, expectedCode, response.Code)
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			//Arrange
+			hd := NewSellerHandler(test.service)
+			hdFunc := hd.GetAll()
+			//Act
+			request := httptest.NewRequest("GET", "/", strings.NewReader(""))
+			request.Header.Set("Content-Type", "application/json")
+			response := httptest.NewRecorder()
+			hdFunc(response, request)
+
+			//Assert
+			test.assertFunc(t, response)
+			require.Equal(t, 1, test.service.Spy.CountGetAllFunc)
+		})
+	}
+}
