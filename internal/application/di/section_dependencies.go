c44b3df140b27af05c9fc8bb9f338ef251e588c9
adgalvan_meli <adriel.galvan@mercadolibre.com>
2025-07-24 11:35:00 -0300

    refactor: reorganize Section handler into dedicated subdirectory
    - Move section_handler.go and section_handler_test.go to internal/handler/section/
    - Update package declarations to 'section'
    - Update imports in router and dependency injection
    - Maintain all existing functionality and tests
    - Improve code organization and maintainability

--- internal/application/di/section_dependencies.go
+++ internal/application/di/section_dependencies.go
@@ -1,15 +1,15 @@
 package di
 
 import (
-	"ProyectoFinal/internal/handler"
+	sectionHandler "ProyectoFinal/internal/handler/section"
 	repository "ProyectoFinal/internal/repository/section"
 	service "ProyectoFinal/internal/service/section"
 	"database/sql"
 )
 
-func GetSectionHandler(db *sql.DB) *handler.SectionDefault {
+func GetSectionHandler(db *sql.DB) *sectionHandler.SectionDefault {
 	sectionRepository := repository.NewSectionMySQL(db)
 	sectionService := service.NewSectionDefault(sectionRepository)
-	sectionHandler := handler.NewSectionDefault(sectionService)
+	sectionHandler := sectionHandler.NewSectionDefault(sectionService)
 	return sectionHandler
 }
--- internal/handler/section/section_handler_test.go
+++ internal/handler/section/section_handler_test.go
@@ -1,4 +1,4 @@
-package handler
+package section
 
 import (
 	"ProyectoFinal/mocks"
