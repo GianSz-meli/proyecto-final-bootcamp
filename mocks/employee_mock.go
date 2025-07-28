4909bcd965cc8625f9ccd0eaabb4e17edbf50bed
omolinaspublic <octavio.molinas@mercadolibre.com>
2025-07-28 19:30:14 -0300

    Add  inbound order service tests

--- mocks/employee_mock.go
+++ mocks/employee_mock.go
@@ -35,3 +35,36 @@ func (m *MockEmployeeService) Delete(id int) error {
 	args := m.Called(id)
 	return args.Error(0)
 }
+
+// MockEmployeeRepository is a mock implementation of the employee.Repository interface
+type MockEmployeeRepository struct {
+	mock.Mock
+}
+
+func (m *MockEmployeeRepository) GetAll() ([]models.Employee, error) {
+	args := m.Called()
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).([]models.Employee), args.Error(1)
+}
+
+func (m *MockEmployeeRepository) GetById(id int) (models.Employee, error) {
+	args := m.Called(id)
+	return args.Get(0).(models.Employee), args.Error(1)
+}
+
+func (m *MockEmployeeRepository) Create(employee *models.Employee) error {
+	args := m.Called(employee)
+	return args.Error(0)
+}
+
+func (m *MockEmployeeRepository) Update(id int, employee models.Employee) error {
+	args := m.Called(id, employee)
+	return args.Error(0)
+}
+
+func (m *MockEmployeeRepository) Delete(id int) error {
+	args := m.Called(id)
+	return args.Error(0)
+}
