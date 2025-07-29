44d36c9ae2882b98ce6d0dccc9ba026e37eadf5c
laralanari <lara.lanari@mercadolibre.com>
2025-07-28 23:48:37 -0300

    product service test done

--- mocks/products_mock.go
+++ mocks/products_mock.go
@@ -6,6 +6,39 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+type MockProductRepository struct {
+	mock.Mock
+}
+
+func (m *MockProductRepository) ExistsProdCode(prodCode string) bool {
+	args := m.Called(prodCode)
+	return args.Bool(0)
+}
+
+func (m *MockProductRepository) CreateProduct(newProd models.Product) (models.Product, error) {
+	args := m.Called(newProd)
+	return args.Get(0).(models.Product), args.Error(1)
+}
+
+func (m *MockProductRepository) FindAllProducts() (map[int]models.Product, error) {
+	args := m.Called()
+	return args.Get(0).(map[int]models.Product), args.Error(1)
+}
+
+func (m *MockProductRepository) FindProductsById(id int) (models.Product, error) {
+	args := m.Called(id)
+	return args.Get(0).(models.Product), args.Error(1)
+}
+
+func (m *MockProductRepository) UpdateProduct(id int, prod models.Product) (models.Product, error) {
+	args := m.Called(id, prod)
+	return args.Get(0).(models.Product), args.Error(1)
+}
+
+func (m *MockProductRepository) DeleteProduct(id int) {
+	m.Called(id)
+}
+
 type MockProductService struct {
 	mock.Mock
 }
