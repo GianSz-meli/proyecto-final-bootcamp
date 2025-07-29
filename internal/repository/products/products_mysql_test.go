5c9d357d14258b0b57b0bba78c8c58504465e84f
laralanari <lara.lanari@mercadolibre.com>
2025-07-29 13:34:57 -0300

    add tests products repository

--- internal/repository/products/products_mysql_test.go
+++ internal/repository/products/products_mysql_test.go
@@ -3,6 +3,7 @@ package products
 import (
 	"ProyectoFinal/pkg/models"
 	"database/sql"
+	"errors"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -12,34 +13,67 @@ import (
 func ptrInt(v int) *int { return &v }
 
 func TestMySQLRepository_CreateProduct(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	require.NoError(t, err)
-	defer db.Close()
+	t.Run("should return product successfully when product is created", func(t *testing.T) {
+		db, mock, err := sqlmock.New()
+		require.NoError(t, err)
+		defer db.Close()
 
-	repo := NewProductSQL(db)
+		repo := NewProductSQL(db)
 
-	product := models.Product{
-		ID:             1,
-		ProductCode:    "A1000",
-		Description:    "Coca Cola 2L",
-		Width:          5,
-		Height:         20,
-		Length:         12,
-		NetWeight:      1.9,
-		ExpirationRate: 15,
-		Temperature:    10,
-		FreezingRate:   10,
-		ProductTypeID:  83,
-		SellerID:       ptrInt(7),
-	}
+		product := models.Product{
+			ID:             1,
+			ProductCode:    "A1000",
+			Description:    "Coca Cola 2L",
+			Width:          5,
+			Height:         20,
+			Length:         12,
+			NetWeight:      1.9,
+			ExpirationRate: 15,
+			Temperature:    10,
+			FreezingRate:   10,
+			ProductTypeID:  83,
+			SellerID:       ptrInt(7),
+		}
 
-	mock.ExpectExec("INSERT INTO products").WithArgs(product.ProductCode, product.Description, product.Width, product.Height, product.Length, product.NetWeight, product.ExpirationRate, product.Temperature, product.FreezingRate, product.ProductTypeID, product.SellerID).WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectExec("INSERT INTO products").WithArgs(product.ProductCode, product.Description, product.Width, product.Height, product.Length, product.NetWeight, product.ExpirationRate, product.Temperature, product.FreezingRate, product.ProductTypeID, product.SellerID).WillReturnResult(sqlmock.NewResult(1, 1))
 
-	result, err := repo.CreateProduct(product)
-	require.NoError(t, err)
-	require.Equal(t, product, result)
+		result, err := repo.CreateProduct(product)
+		require.NoError(t, err)
+		require.Equal(t, product, result)
 
-	mock.ExpectationsWereMet()
+		mock.ExpectationsWereMet()
+	})
+	t.Run("should return error when LastInsertId fails", func(t *testing.T) {
+		db, mock, err := sqlmock.New()
+		require.NoError(t, err)
+		defer db.Close()
+
+		repo := NewProductSQL(db)
+		product := models.Product{
+			ID:             1,
+			ProductCode:    "A1000",
+			Description:    "Coca Cola 2L",
+			Width:          5,
+			Height:         20,
+			Length:         12,
+			NetWeight:      1.9,
+			ExpirationRate: 15,
+			Temperature:    10,
+			FreezingRate:   10,
+			ProductTypeID:  83,
+			SellerID:       ptrInt(7),
+		}
+
+			mock.ExpectExec("INSERT INTO products").
+				WithArgs(product.ProductCode, product.Description, product.Width, product.Height, product.Length, product.NetWeight, product.ExpirationRate, product.Temperature, product.FreezingRate, product.ProductTypeID, product.SellerID).
+				WillReturnResult(sqlmock.NewErrorResult(errors.New("LastInsertId failed")))
+
+		result, err := repo.CreateProduct(product)
+		require.Error(t, err)
+		require.Equal(t, product, result)
+
+		mock.ExpectationsWereMet()
+	})
 }
 
 func TestMySQLRepository_FindAllProducts(t *testing.T) {
@@ -79,35 +113,35 @@ func TestMySQLRepository_FindAllProducts(t *testing.T) {
 
 func TestMySQLRepository_FindProductsById(t *testing.T) {
 	t.Run("should return product successfully when product exists", func(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	require.NoError(t, err)
-	defer db.Close()
+		db, mock, err := sqlmock.New()
+		require.NoError(t, err)
+		defer db.Close()
 
-	repo := NewProductSQL(db)
+		repo := NewProductSQL(db)
 
-	product := models.Product{
-		ID:             1,
-		ProductCode:    "A1000",
-		Description:    "Coca Cola 2L",
-		Width:          5,
-		Height:         20,
-		Length:         12,
-		NetWeight:      1.9,
-		ExpirationRate: 15,
-		Temperature:    10,
-		FreezingRate:   10,
-		ProductTypeID:  83,
-		SellerID:       ptrInt(7),
-	}
+		product := models.Product{
+			ID:             1,
+			ProductCode:    "A1000",
+			Description:    "Coca Cola 2L",
+			Width:          5,
+			Height:         20,
+			Length:         12,
+			NetWeight:      1.9,
+			ExpirationRate: 15,
+			Temperature:    10,
+			FreezingRate:   10,
+			ProductTypeID:  83,
+			SellerID:       ptrInt(7),
+		}
 
-	rows := sqlmock.NewRows([]string{"id", "product_code", "description", "width", "height", "length", "net_weight", "expiration_rate", "recommended_freezing_temperature", "freezing_rate", "product_type_id", "seller_id"}).
-		AddRow(product.ID, product.ProductCode, product.Description, product.Width, product.Height, product.Length, product.NetWeight, product.ExpirationRate, product.Temperature, product.FreezingRate, product.ProductTypeID, product.SellerID)
+		rows := sqlmock.NewRows([]string{"id", "product_code", "description", "width", "height", "length", "net_weight", "expiration_rate", "recommended_freezing_temperature", "freezing_rate", "product_type_id", "seller_id"}).
+			AddRow(product.ID, product.ProductCode, product.Description, product.Width, product.Height, product.Length, product.NetWeight, product.ExpirationRate, product.Temperature, product.FreezingRate, product.ProductTypeID, product.SellerID)
 
-	mock.ExpectQuery("SELECT id, product_code, description, width, height, length, net_weight, expiration_rate, recommended_freezing_temperature, freezing_rate, product_type_id, seller_id FROM products WHERE id = ?").WithArgs(1).WillReturnRows(rows)
+		mock.ExpectQuery("SELECT id, product_code, description, width, height, length, net_weight, expiration_rate, recommended_freezing_temperature, freezing_rate, product_type_id, seller_id FROM products WHERE id = ?").WithArgs(1).WillReturnRows(rows)
 
-	result, err := repo.FindProductsById(1)
-	require.NoError(t, err)
-	require.Equal(t, product, result)
+		result, err := repo.FindProductsById(1)
+		require.NoError(t, err)
+		require.Equal(t, product, result)
 
 		mock.ExpectationsWereMet()
 	})
@@ -127,6 +161,24 @@ func TestMySQLRepository_FindProductsById(t *testing.T) {
 
 		mock.ExpectationsWereMet()
 	})
+	t.Run("should return error when database query fails", func(t *testing.T) {
+		db, mock, err := sqlmock.New()
+		require.NoError(t, err)
+		defer db.Close()
+
+		repo := NewProductSQL(db)
+
+		mock.ExpectQuery("SELECT id, product_code, description, width, height, length, net_weight, expiration_rate, recommended_freezing_temperature, freezing_rate, product_type_id, seller_id FROM products WHERE id = ?").
+			WithArgs(1).
+			WillReturnError(errors.New("database connection failed"))
+
+		result, err := repo.FindProductsById(1)
+		require.Error(t, err)
+		require.Equal(t, models.Product{}, result)
+
+		mock.ExpectationsWereMet()
+	})
+
 }
 
 func TestMySQLRepository_UpdateProduct(t *testing.T) {
